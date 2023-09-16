8d2abd807b6860334f65857eebed083bc79695e2
Nick Yadlouski <yadlouskin@gmail.com>
2023-09-16 18:04:04 +0300

    Refactor Configuration to add MongoDb settings
    Change appsettings.json structure and add new fields for MongoDb
    Add Mongo's Url, Database and Collection to Configuration class

--- QueryBuilder/Configuration.cs
+++ QueryBuilder/Configuration.cs
@@ -18,9 +18,21 @@ namespace QueryBuilder
 
         /// <summary>
         /// Connection string to MongoDb
-        /// Default value: ""
+        /// Default value: "mongodb://127.0.0.1:27017/"
         /// </summary>
-        public string MongoDbConnectionString { get; private set; }
+        public string MongoUrl { get; private set; }
+
+        /// <summary>
+        /// Database name in MongoDb for QueryBuilder application
+        /// Default value: "querybuilder"
+        /// </summary>
+        public string MongoDatabase { get; private set; }
+
+        /// <summary>
+        /// Collection name from Mongo database
+        /// Default value: "sale"
+        /// </summary>
+        public string MongoCollection { get; private set; }
 
         private Configuration()
         {
@@ -29,8 +41,10 @@ namespace QueryBuilder
             // TODO: We can use try..catch and assign default value, but in any case the default value will not work everywhere
             // TODO: We can add possibility to read values from environment variables or from command line arguments
             var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            QueryBuilderUrl = config.GetSection("ConnectionStrings")["QueryBuilderUrl"] ?? "http://localhost:8000/";
-            MongoDbConnectionString = config.GetSection("ConnectionStrings")["MongoDbConnectionString"] ?? "";
+            QueryBuilderUrl = config.GetSection("QueryBuilder")["Url"] ?? "http://localhost:8000/";
+            MongoUrl = config.GetSection("MongoDb")["Url"] ?? "mongodb://127.0.0.1:27017/";
+            MongoDatabase = config.GetSection("MongoDb")["Database"] ?? "querybuilder";
+            MongoCollection = config.GetSection("MongoDb")["Collection"] ?? "sale";
         }
 
         private static Configuration? instance = null;
