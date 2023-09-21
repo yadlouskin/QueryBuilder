fc2898d5a3844bfc859c82ebd15c405185abcd3e
Nick Yadlouski <yadlouskin@gmail.com>
2023-09-21 18:12:37 +0300

    Add function to create filter with entering value

--- QueryBuilder/DataModel.cs
+++ QueryBuilder/DataModel.cs
@@ -31,6 +31,7 @@ namespace QueryBuilder
             {
                 string name = property.Key;
                 string type = property.Value;
+                GenerateFilter(name, type);
             }
 
             // Add stub filter to ensure the application works
@@ -39,6 +40,49 @@ namespace QueryBuilder
                 return "{id: 'name',\n label: 'name',\n type: 'string'}";
             }
             return string.Join(",", Filters);
+        }
+
+        private bool GenerateFilter(string name, string type)
+        {
+            string filter = string.Format("{{\n id: '{0}'"
+                + ",\n field: '{0}'"
+                + ",\n label: '{0}'"
+                , name);
+
+            // TODO: Add field and create different filters with the same field
+            switch (type)
+            {
+                case "Int32":
+                    filter += ",\n type: 'integer'";
+                    break;
+
+                case "Int16":
+                    filter += ",\n type: 'integer'";
+                    break;
+
+                case "Bool":
+                    filter += ",\n type: 'boolean'";
+                    break;
+
+                case "String":
+                    break;
+
+                case "Date":
+                    filter += ",\n type: 'date'";
+                    filter += ",\n validation: {format: 'YYYYMMDD'}";
+                    filter += ",\n plugin: 'datepicker'";
+                    filter += ",\n plugin_config: {\n format: 'yyyymmdd', \n" +
+                        "todayBtn: 'linked',\n todayHighlight: true,\n autoclose: true \n}";
+                    break;
+
+                default:
+                    filter += ",\n type: 'string'";
+                    break;
+            }
+
+            filter += "\n}";
+            Filters.Add(filter);
+            return true;
         }
 
     }
