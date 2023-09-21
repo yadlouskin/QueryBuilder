45edc1c030702ad13e58c8fdbd1d2ef38ea4e031
Nick Yadlouski <yadlouskin@gmail.com>
2023-09-21 17:56:12 +0300

    Create test data to check functionality

---
+++ QueryBuilder/TestModelData.cs
@@ -0,0 +1,70 @@
+namespace QueryBuilder
+{
+
+/*
+{
+    Name: "Minsk",
+    Population: 1995000,
+    CityDate: 20230909,
+    BasedDate: 10670101,
+    Famous: ["National library", "Troitskoe", "Nemiga"],
+    Structure:
+    {
+        Districsts: 9,
+        MainStreet: "Independence Avenue",
+        Rivers:[
+            {
+                Name: "Svislach",
+                Length: 327,
+                Navigable: true
+            }, {
+                Name: "Nemiga",
+                Length: 5,
+                Navigable: false
+            }
+        ]
+    }
+},{
+    Name: "Brest",
+    Population: 340000,
+    CityDate: 20230728,
+    BasedDate: 10170101,
+    Famous: ["Brest Fortress"],
+    Structure:
+    {
+        Districsts: 2,
+        MainStreet: "Masherov Avenue",
+        Rivers:[
+            {
+                Name: "Bug",
+                Length: 774,
+                Navigable: true
+            }, {
+                Name: "Mukhavets",
+                Length: 113,
+                Navigable: true
+            }
+        ]
+    }
+},{
+    Name: "Vitebsk",
+    Population: 360000,
+    CityDate: 20230624,
+    BasedDate: 10210101,
+    Famous: ["Summer Amphitheatre", "Slavic Bazaar"],
+    Structure:
+    {
+        Districsts: 3,
+        MainStreet: "Leningradskaya Street",
+        Rivers:[
+            {
+                Name: "Western Dvina",
+                Length: 1020,
+                Navigable: true
+            }
+        ]
+    }
+}
+*/
+
+}
